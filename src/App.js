178754ca0c1309622a9ac040c409f599c658dbec
danielvebo90 <danielvebo@gmail.com>
2020-02-17 22:08:03 -0500

    1 repo

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.valor = 0
+    this.state = {
+      value: this.valor
+    }
+
+  }
+
+  render() {
+    return (
+      <div>
+        <span className="value">{this.state.value}</span>
+        <button id="inc" onClick={this.addOne.bind(this)}>Incrementa</button>
+      </div>
+    );
+  }
+
+  addOne() {
+    this.valor ++
+    this.setState({
+      value: this.valor
+    });
+  }
+
+
+
+}
+
+export default App;
